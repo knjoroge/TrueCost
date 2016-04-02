04da7ebb79e6a08b4603994358ec5d46d842b620
Hao Chen <detectx@gmail.com>
2016-04-02 14:56:50 -0400

    Split pages into product, add to cart, checkout

--- user.js
+++ user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         True Cost
-// @namespace    http://tampermonkey.net/
+// @namespace    http://github.com/truecost4env/
 // @version      0.1
-// @description  Display true cost of product
+// @description  Display true cost of product and allow user to offset environmental impacts
 // @author       True Cost
 // @match        http*://www.amazon.com/*/dp/*
+// @match        http*://www.amazon.com/*/huc/*
+// @match        http*://www.amazon.com/*/buy/*
 // @require      https://code.jquery.com/jquery-2.2.2.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/accounting.js/0.4.1/accounting.min.js
 // @resource css https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css
@@ -15,29 +17,46 @@
 (function() {
   'use strict';
 
+  // Insert custom CSS
   var css = GM_getResourceText("css");
   GM_addStyle(css);
 
-  var $cost = $('#priceblock_ourprice'),
-      cost = accounting.unformat($cost.text()),
-      trueCost = accounting.formatMoney(cost * 2),
-      $addToCart = $('#add-to-cart-button').closest('.a-button-stack');
+  var isProductPage = /\/dp\//.test(location.href),
+      isAddToCartPage = /\/huc\//.test(location.href),
+      isCheckoutPage = /\/buy\//.test(location.href);
 
-  var $icoCarbon = $('<i/>').addClass('fa fa-cloud'),
-      $icoEnergy = $('<i/>').addClass('fa fa-bolt'),
-      $icoWaste = $('<i/>').addClass('fa fa-trash-o'),
-      $icoChem = $('<i/>').addClass('fa fa-flask');
+  // Product page
+  if(isProductPage){
+      // Get cost
+      var $cost = $('#priceblock_ourprice'),
+          cost = accounting.unformat($cost.text()),
+          trueCost = accounting.formatMoney(cost * 2),
+          $addToCart = $('#add-to-cart-button').closest('.a-button-stack');
 
-  var $trueCost = $('<span/>')
-    .text("True Cost: " + trueCost)
-    .addClass('a-size-medium')
-    .css('color', 'green')
-    .css('padding', '0 10px');
+      var $icoCarbon = $('<i/>').addClass('fa fa-cloud'),
+          $icoEnergy = $('<i/>').addClass('fa fa-bolt'),
+          $icoWaste = $('<i/>').addClass('fa fa-trash-o'),
+          $icoChem = $('<i/>').addClass('fa fa-flask');
 
-  $addToCart
-    .after($trueCost)
-    .after($icoCarbon)
-    .after($icoEnergy)
-    .after($icoWaste)
-    .after($icoChem);
+      var $trueCost = $('<span/>')
+      .text("True Cost: " + trueCost)
+      .addClass('a-size-medium')
+      .css('color', 'green')
+      .css('padding', '0 10px');
+
+      $addToCart
+          .after($trueCost)
+          .after($icoCarbon)
+          .after($icoEnergy)
+          .after($icoWaste)
+          .after($icoChem);
+
+  // Add Product To Cart page
+  }else if(isAddToCartPage){
+    console.log('added product to cart');
+
+  // Checkout page
+  }else if(isCheckoutPage){
+    console.log('checkout page');
+  }
 })();
