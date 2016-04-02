c0c5e34baae70c76b864074406a18daa4027ad66
Hao Chen <detectx@gmail.com>
2016-04-02 14:38:03 -0400

    Add icons, use Add to Cart button

--- user.js
+++ user.js
@@ -4,22 +4,40 @@
 // @version      0.1
 // @description  Display true cost of product
 // @author       True Cost
-// @match        http*://www.amazon.com/dp/B016CDOPAO/*
+// @match        http*://www.amazon.com/*/dp/*
 // @require      https://code.jquery.com/jquery-2.2.2.min.js
-// @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/accounting.js/0.4.1/accounting.min.js
+// @resource css https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  var $price = $('#priceblock_ourprice');
-  var $realPrice = $('<span/>')
-    .text('$50.00')
+  var css = GM_getResourceText("css");
+  GM_addStyle(css);
+
+  var $cost = $('#priceblock_ourprice'),
+      cost = accounting.unformat($cost.text()),
+      trueCost = accounting.formatMoney(cost * 2),
+      $addToCart = $('#add-to-cart-button').closest('.a-button-stack');
+
+  var $icoCarbon = $('<i/>').addClass('fa fa-cloud'),
+      $icoEnergy = $('<i/>').addClass('fa fa-bolt'),
+      $icoWaste = $('<i/>').addClass('fa fa-trash-o'),
+      $icoChem = $('<i/>').addClass('fa fa-flask');
+
+  var $trueCost = $('<span/>')
+    .text("True Cost: " + trueCost)
     .addClass('a-size-medium')
     .css('color', 'green')
     .css('padding', '0 10px');
 
-  $price
-    .css('text-decoration', 'line-through')
-    .after($realPrice);
+  $addToCart
+    .after($trueCost)
+    .after($icoCarbon)
+    .after($icoEnergy)
+    .after($icoWaste)
+    .after($icoChem);
 })();
